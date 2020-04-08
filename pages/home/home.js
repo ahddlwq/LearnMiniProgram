c4e5ffeaee1bc4420842cc57172ad1029a71b66e
ahddlwq <597122886@qq.com>
2020-04-08 11:23:56 +0800

    知识点12

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,66 +1,43 @@
 // pages/home/home.js
+//getApp()可以获取App()产生的实例对象
+const app = getApp()
+console.log(app.globalData.name)
+console.log(app.globalData.age)
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  //-----------1.初始化数据-------------
+  data:{
+    message:'哈哈哈',
+    array:[1,2,3,4,5,6,7,8,9,10]
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  //----2.监听wxml中相关的一些事件-------
+  handleGetUserInfo(event){
+    console.log(event)
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleViewClick(){
+    console.log('哈哈哈被点击了')
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  //---------3.监听生命周期函数----------
+  onLoad:()=>{
+    console.log('onLoad')
+    wx.request({
+      url: 'http://123.207.32.32:8000/recommend',
+      success:(res)=>{
+        console.log(res)
+      }
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  //-------4.监听其它事件如页面滚动等--------
+  //监听页面的滚动
+  onPageScroll(obj){
+    console.log(obj)
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  //监听页面滚动到底部
+  onReachBottom() {
+    console.log('页面滚动到底部')
   },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  //监听页面下拉刷新
+  onPullDownRefresh() {
+    console.log('监听页面下拉刷新')
   }
 })
