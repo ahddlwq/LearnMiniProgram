211aa5d6928e1aae3a71528d35dcdb33446f490d
ahddlwq <597122886@qq.com>
2020-07-08 10:58:50 +0800

    知识点35

--- pages/home/home.js
+++ pages/home/home.js
@@ -5,8 +5,66 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    price:"27.666666",
+    time: 1560598160,
+    titles:["衣服","裤子","鞋子"]
   },
+  handleBtnClick(){
+    console.log("按钮被点击！");
+  },
+  handleTouchStart() {
+    console.log("handleTouchStart");
+  },
+  handleTouchMove() {
+    console.log("handleTouchMove");
+  },
+  handleTouchEnd() {
+    console.log("handleTouchEnd");
+  },
+  handleLongPress() {
+    console.log("handleLongPress");
+  },
+  handleTap() {
+    console.log("handleTap");
+  },
+  handleMyBtnClick(e) {
+    console.log(e);
+  },
+  handleOuter(event){
+    console.log(event);
+  },
+  handleInner(event) {
+    console.log(event);
+  },
+  handleItemClick(event) {
+    //console.log(event);
+    const dataset = event.currentTarget.dataset;
+    const title = dataset.item;
+    const index = dataset.index;
+    console.log(title,index);
+  },
+
+  handleCaptureView1(){
+    console.log("handleCaptureView1");
+  },
+  handleBindView1() {
+    console.log("handleBindView1");
+  },
+  handleCaptureView2() {
+    console.log("handleCaptureView2");
+  },
+  handleBindView2() {
+    console.log("handleBindView2");
+  },
+  handleCaptureView3() {
+    console.log("handleCaptureView3");
+  },
+  handleBindView3() {
+    console.log("handleBindView3");
+  },
+
+
+
 
   /**
    * 生命周期函数--监听页面加载
