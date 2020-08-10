42b07c6269021252947ab8d027288f531b81a468
ahddlwq <597122886@qq.com>
2020-08-10 16:33:33 +0800

    知识点46

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,4 +1,6 @@
 // pages/home/home.js
+import request from '../../service/network.js'
+
 Page({
 
   /**
@@ -12,7 +14,56 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    //1.没有参数的网络请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/recommend',
+    //   success:function(res){
+    //     console.log(res)
+    //   }
+    // })
 
+    //2.有参数的网络请求
+    // wx.request({
+    //   url: 'http://123.207.32.32:8000/home/data',
+    //   data:{
+    //     type:'sell',
+    //     page:1        
+    //   },
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+
+    //3.post请求
+    // wx.request({
+    //   url: 'http://httpbin.org/post',
+    //   data: {
+    //     type: 'why',
+    //     age:18,
+    //     height: 1.88
+    //   },
+    //   method:'post',
+    //   success: function (res) {
+    //     console.log(res)
+    //   }
+    // })
+
+     //4.通过自己封装的request发送网络请求
+     //Promise最大好处就是防止出现回调地狱
+    //  request({
+    //    url: 'http://123.207.32.32:8000/recommend'
+    //  })
+     
+     
+     //5.通过自己封装的request发送网络请求
+     //Promise最大好处就是防止出现回调地狱
+    request({
+      url: 'http://123.207.32.32:8000/recommend'
+    }).then(res => {
+      console.log(res)
+    }).catch(err => {
+       console.log(err)
+    })    
   },
 
   /**
